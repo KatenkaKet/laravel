c71433dfdb5b62d1638fbc623c2bbe64e8857487
KatenkaKet <katenkeket13@gmail.com>
2025-02-17 17:15:42 +0500

    Установка anctum

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/corpuses', [\App\Http\Controllers\CorpusControllerApi::class, 'index']);
 Route::get('/corpus/{id}', [\App\Http\Controllers\CorpusControllerApi::class, 'show']);
 
-Route::get('/room', [\App\Http\Controllers\RoomControllerApi::class, 'index']);
+//Route::middleware('auth:sanctum')->get('/room', [\App\Http\Controllers\RoomControllerApi::class, 'index']);
 Route::get('/room/{id}', [\App\Http\Controllers\RoomControllerApi::class, 'show']);
 
 Route::get('/guest/', [\App\Http\Controllers\GuestControllerApi::class, 'index']);
 Route::get('/guest/{id}', [\App\Http\Controllers\GuestControllerApi::class, 'show']);
+
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+//Route::middleware('auth:sanctum')->get('/user', function(Request $request){
+//   return $request->user();
+//});
+
+//Route::middleware('auth:sanctum')->get('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::get('/room', [\App\Http\Controllers\RoomControllerApi::class, 'index']);
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
+    Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+});
